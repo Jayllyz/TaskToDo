389d872b78898f288e485d37d3280390096f1699
Jayllyz <antonydavid945@gmail.com>
2022-12-01 08:52:10 +0100

    refactor

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
    gtk_init(&argc, &argv);
 
    builder = gtk_builder_new();
-   gtk_builder_add_from_file(builder, "window_main.glade", NULL);
+   gtk_builder_add_from_file(builder, "data/window_main.glade", NULL);
 
    window =
        GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
