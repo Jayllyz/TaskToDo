f7f6aa82d42b7023e68ba92b23cfc23caa07675c
Simon <szhang0709@gmail.com>
2022-12-03 00:53:01 +0100

    Add button click function

--- main.c
+++ main.c
@@ -5,18 +5,28 @@ Description: Main file of our Todo list software
 */
 
 #include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+    //J'ai declare une variable en global, faut surement pas faire mais je sais pas comment envoyer cette
+    //variable en parametre dans la fonction click_projects en appuyant dans le bouton
+    //je verrai surement plus tard
+    GtkLabel *label_test;
 
 int main(int argc, char *argv[]) {
    GtkBuilder *builder;
-   GtkWidget *window;
 
    gtk_init(&argc, &argv);
 
    builder = gtk_builder_new();
    gtk_builder_add_from_file(builder, "data/window_main.glade", NULL);
 
-   window =
-       GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    //Variables
+    
+    GtkWidget *window;
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    label_test = GTK_LABEL(gtk_builder_get_object(builder, "test_label"));
+
    gtk_builder_connect_signals(builder, NULL);
 
    g_object_unref(builder);
@@ -29,3 +39,8 @@ int main(int argc, char *argv[]) {
 
 // called when window is closed
 void on_window_main_destroy() { gtk_main_quit(); }
+
+void click_projects(){
+    printf("test");
+    gtk_label_set_text(label_test, "Projet ajouté");
+}
