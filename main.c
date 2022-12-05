a5f825593012e65c12fe9a30dda8e5172e6c89f0
Antony David <antonydavid945@gmail.com>
2022-12-05 15:42:38 +0100

    get input in textView
    NOT WORKING (SEG FAULT)

--- main.c
+++ main.c
@@ -4,39 +4,39 @@ Date: 24-11-2022
 Description: Main file of our Todo list software
 */
 
+#include "functions.c"
+#include "functions.h"
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-void click_projects(GtkLabel *label, gpointer data)
-{
-    gtk_label_set_text(label, "Projet ajouté");
-}
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    GtkBuilder *builder;
-    GtkWidget *event_box;
-    GtkLabel *label_test;
-    GtkButton *addProjects;
+
+    //Init
     gtk_init(&argc, &argv);
+    struct data user;
 
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "data/window_main.glade", NULL);
+    user.builder = gtk_builder_new();
+    gtk_builder_add_from_file(user.builder, "data/window_main.glade", NULL);
 
-    //Variables
-    GtkWidget *window;
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
-    label_test = GTK_LABEL(gtk_builder_get_object(builder, "test_label"));
-    addProjects = GTK_BUTTON(gtk_builder_get_object(builder, "add_projects"));
+    //Datas
+    user.window = GTK_WIDGET(gtk_builder_get_object(user.builder, "window_main"));
+    user.label_test = GTK_LABEL(gtk_builder_get_object(user.builder, "test_label"));
+    user.labeltext = GTK_LABEL(gtk_builder_get_object(user.builder, "labeltext"));
+    user.addProjects = GTK_BUTTON(gtk_builder_get_object(user.builder, "add_projects"));
+    user.refresh = GTK_BUTTON(gtk_builder_get_object(user.builder, "refresh")); //refresh labeltext
 
-    g_signal_connect_swapped(addProjects, "clicked", G_CALLBACK(click_projects), label_test);
+    //signals
+    g_signal_connect_swapped(user.addProjects, "clicked", G_CALLBACK(click_projects), user.label_test);
+    g_signal_connect(user.refresh, "clicked", G_CALLBACK(refreshButton), &user);
 
-    gtk_builder_connect_signals(builder, NULL);
+    gtk_builder_connect_signals(user.builder, NULL);
 
-    g_object_unref(builder);
+    g_object_unref(user.builder);
 
-    gtk_widget_show(window);
+    gtk_widget_show(user.window);
     gtk_main();
 
     return 0;
