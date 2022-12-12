0446ebf4e2a290cf4b36f6f683e655b3d35c276e
Antony David <antonydavid945@gmail.com>
2022-12-12 17:36:46 +0100

    create tables

--- settings/bdd.c
+++ settings/bdd.c
@@ -1,29 +1,61 @@
+#include "../functions.h"
+#include <libpq-fe.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <libpq-fe.h>
 
-void do_exit(PGconn *conn) {
+void bddExist(PGconn *conn, PGresult *res)
+{
+    fprintf(stderr, "%s\n", PQerrorMessage(conn));    
+
+    PQclear(res);
+    PQfinish(conn);    
     
-    PQfinish(conn);
     exit(1);
 }
 
-int main() {
-    
+PGconn *connectBdd()
+{
     PGconn *conn = PQconnectdb("user=projet password=Respons11 dbname=projet-todolist");
-
     if (PQstatus(conn) == CONNECTION_BAD) {
-        
-        fprintf(stderr, "Connection to database failed: %s\n",
-            PQerrorMessage(conn));
-        do_exit(conn);
+
+        return NULL;
+    }
+    return conn;
+}
+
+int createTables(PGconn *conn)
+{
+
+    PGresult *res = PQexec(conn, "DROP TABLE IF EXISTS Cars");
+
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        bddExist(conn, res);
     }
 
-    int ver = PQserverVersion(conn);
+    PQclear(res);
 
-    printf("Server version: %d\n", ver);
-    
-    PQfinish(conn);
+    res = PQexec(conn,
+        "CREATE TABLE Cars(Id INTEGER PRIMARY KEY,"
+        "Name VARCHAR(20), Price INT)");
 
-    return 0;
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        bddExist(conn, res);
+    }
+
+    PQclear(res);
+
+    res = PQexec(conn, "INSERT INTO Cars VALUES(1,'Audi',52642)");
+
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+        bddExist(conn, res);
+
+    PQclear(res);
+
+    res = PQexec(conn, "INSERT INTO Cars VALUES(2,'Mercedes',57127)");
+
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        bddExist(conn, res);
+    }
+
+    PQclear(res);
 }
