436d00d47d0ec37637b5391df21af95651b21bcb
Antony David <antonydavid945@gmail.com>
2022-12-31 20:23:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -178,7 +178,7 @@ int main(int argc, char *argv[])
     g_signal_connect(data.tools.setExpense, "clicked", G_CALLBACK(financeButton), &data);
     g_signal_connect(data.tools.desetExpense, "clicked", G_CALLBACK(financeButton), &data);
 
-    // g_signal_connect(data.tools.window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(data.tools.window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_builder_connect_signals(data.tools.builder, NULL);
 
     g_object_unref(data.tools.builder);
