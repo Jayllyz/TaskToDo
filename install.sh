727bf2e8f5f910d0aff587e195bc5f3e2d538c94
Antony David <antonydavid945@gmail.com>
2022-12-31 16:00:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,27 +6,35 @@
 #dos2unix install.sh
 
 # Upgrade the package list
-sudo apt update && sudo apt upgrade -y
+sudo apt update -y 
+sudo apt upgrade -y
 sudo apt install build-essential -y
+
+# Install CMake
+sudo apt install cmake -y
 
 # If you want to install clang to format the code 
 # automatically with CMake, uncomment line below.
 #sudo apt install clang -y 
 
 # Install GTK+3.2
-sudo apt install libgtk-3-dev
+sudo apt install libgtk-3-dev -y
 
 # Install cURL
-sudo apt install curl && sudo apt install libcurl4-openssl-dev
+sudo apt install curl -y
+sudo apt install libcurl4-openssl-dev -y
 
 # Install Glade
-sudo apt install glade
+sudo apt install glade -y
 
 # Install postgresql and libpq
-sudo apt install postgresql && sudo apt install libpq-dev
+sudo apt install postgresql -y
+sudo apt install libpq-dev -y
 
 # Configure postgresql
 sudo -i -u postgres psql -c "CREATE USER projet WITH PASSWORD 'Respons11';"
 sudo -u postgres createdb projet-todolist --owner projet
+sudo nano /etc/postgresql/14/main/pg_hba.conf # Change peer to scram-sha-256
+sudo service postgresql restart
 
 # You can now compile the project with CMake and run it.
